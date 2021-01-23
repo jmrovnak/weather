0368a9c0a2662e00ab194cc200c1bf7787dcaec0
Justin Rovnak <jmrovnak@gmail.com>
2021-01-22 20:07:51 -0600

    starting js to store city

---
+++ weather.js
@@ -0,0 +1,7 @@
+var APIKey = "3a1ea0eab5865c5368524eab91408d4f";
+
+var input = document.querySelector("#searchInput");
+var search = document.querySelector("#searchButton");
+search.addEventListener("click", function () {
+    console.log(input.value);
+})
