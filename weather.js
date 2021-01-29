a86291bef20a679a020df1db1d1445e6498b25dd
Justin Rovnak <jmrovnak@gmail.com>
2021-01-29 17:53:36 -0600

    converted to f, truncated, populated

--- weather.js
+++ weather.js
@@ -30,11 +30,11 @@ $.ajax ({
     console.log(result.main.humidity);
     console.log(result.wind.speed);
     
-    var tempF = (result.main.temp - 273.15) * 1.80 + 32;
+    let tempF = Math.trunc(result.main.temp - 273.15) * 1.80 + 32;
     console.log(tempF);
 
     $(".display-4").html("<h3>" + city + " Details</h3>");
-    $("#temp").html("<h4>" + result.main.temp + " Degrees Celsius</h4>");
+    $("#temp").html("<h4>" + tempF + " &#xb0 F</h4>");
     $("#humidity").html("<h4>" + result.main.humidity + "%</h4>"); 
     $("#wind").html("<h4>" + result.wind.speed + "MPH</h4>");
     
@@ -59,6 +59,9 @@ $("#searchButton").on("click", function () {
     .then(function(response) {
             console.log(forecastURL);
             console.log(response);
+
+            let tempF = Math.trunc(response.list.temp - 273.15) * 1.80 + 32;
+            console.log(tempF);
 
             console.log(response.list[0].main.temp);
 
